c313b7151ad0c1dc7f43985cf0a0882dda5b3450
Leo Nania <leo.nania18@gmail.com>
2022-01-10 22:50:31 -0300

    Entrega de proyecto clase 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './componentes/Navbar.jsx';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Navbar />
         <p>
           HOLA MUNDO
         </p>
